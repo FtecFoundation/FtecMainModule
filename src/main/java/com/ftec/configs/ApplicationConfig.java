21d41844e983907ee57d273dbd9e34f28187bf69
Ftec <ftecfound@gmail.com>
2018-02-08 15:28:59 +0200

    Initialized project, create starters and project structure

---
+++ src/main/java/com/ftec/configs/ApplicationConfig.java
@@ -0,0 +1,19 @@
+package com.ftec.configs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication(scanBasePackages = {"com.ftec.configs"})
+public class ApplicationConfig extends SpringBootServletInitializer {
+    //Also added configuration for tomcat starting
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApplicationConfig.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ApplicationConfig.class, args);
+    }
+}
