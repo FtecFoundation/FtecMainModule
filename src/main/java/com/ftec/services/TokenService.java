017324e53f3c9f0e3f2b6b1a97483acde29c468c
Dudoser228 <Dudoser228@10.0.1.40>
2018-05-28 07:00:46 -0700

    fixs

--- src/main/java/com/ftec/services/TokenService.java
+++ src/main/java/com/ftec/services/TokenService.java
@@ -84,21 +84,5 @@ public class TokenService {
 		
 		return expirationTime.after(new Date());
 	}
-	
-	public static void main(String[] args) {
-		   	int leftLimit = 97; // letter 'a'
-		    int rightLimit = 122; // letter 'z'
-		    int targetStringLength = 18;
-		    Random random = new Random();
-		    StringBuilder buffer = new StringBuilder(targetStringLength);
-		    for (int i = 0; i < targetStringLength; i++) {
-		        int randomLimitedInt = leftLimit + (int) 
-		          (random.nextFloat() * (rightLimit - leftLimit + 1));
-		        buffer.append((char) randomLimitedInt);
-		    }
-		    String generatedString = buffer.toString();
-		 
-		    System.out.println("22_"+generatedString);
-	}
-	
+
 }
