4fb99e51a45bc44d0f3ff149b2d18492e49e9fce
Roman <kovalovrmn@gmail.com>
2018-05-29 13:02:20 +0300

    Refactoring

--- src/main/java/com/ftec/services/UserServiceImpl.java
+++ src/main/java/com/ftec/services/UserServiceImpl.java
@@ -7,7 +7,6 @@ import com.ftec.services.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
