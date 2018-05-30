24029bd49f5a7f523ea90a36dd83c365278b6425
Roman <kovalovrmn@gmail.com>
2018-05-30 11:11:17 +0300

    Password Utils Test renamed

--- src/test/java/PasswordUtilsTest.java
+++ src/test/java/PasswordUtilsTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 
-public class PasswordEncodeTest {
+public class PasswordUtilsTest {
 
     @Test
     public void isPasswordMatchTest() {
