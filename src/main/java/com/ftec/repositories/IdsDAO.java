357a5115489d330bb2befe0d09d16cf42096ef1f
Ftec <ftecfound@gmail.com>
2018-05-30 13:03:40 +0300

    Id manager increment implementation

--- src/main/java/com/ftec/repositories/IdsDAO.java
+++ src/main/java/com/ftec/repositories/IdsDAO.java
@@ -1,12 +1,15 @@
 package com.ftec.repositories;
 
-import org.springframework.data.elasticsearch.annotations.Query;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
 import com.ftec.entities.Ids;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface IdsDAO extends ElasticsearchRepository<Ids, String> {
     Ids findByTableName(String tableName);
-    @Query(value = "{\"script\": {\"source\": \"ctx._source.lastId++\"}, \"query\": {\"match\": {\"tableName\": \"?0\"}}")
-    void incrementLastId(String tableName);
+
+    //TODO search for more convenient solution
+    default void incrementLastId(String tableName){
+        Ids id = findByTableName(tableName);
+        id.incrementIndex();
+        save(id);
+    };
 }
