4466acb3fee53e95dc414d0ae7d4380f6c90fce9
Roman <kovalovrmn@gmail.com>
2018-05-30 12:06:12 +0300

    Conflicts in User DAO

--- src/main/java/com/ftec/repositories/UserDAO.java
+++ src/main/java/com/ftec/repositories/UserDAO.java
@@ -6,15 +6,9 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import java.util.Optional;
 
 public interface UserDAO extends ElasticsearchRepository<User, Long> {
-<<<<<<< HEAD
 
     @Override
     Optional<User> findById(Long id);
 
     Optional<User> findByUsername(String Username);
-=======
-	 
-	 Optional<User> findById(long id);
-	 Optional<User> findByUsername(String Username);
->>>>>>> 482cb66e412aa950c729f2775de3b8b22049cbec
 }
