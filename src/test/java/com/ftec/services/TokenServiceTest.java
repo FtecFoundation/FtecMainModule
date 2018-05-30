96a0373f5fe2477b4b5c81868c2a5bf0f9aeeed4
Ftec <ftecfound@gmail.com>
2018-05-30 13:15:25 +0300

    Tokens test service injection fix

--- src/test/java/com/ftec/services/TokenServiceTest.java
+++ src/test/java/com/ftec/services/TokenServiceTest.java
@@ -1,18 +1,21 @@
 package com.ftec.services;
+import com.ftec.configs.ApplicationConfig;
+import com.ftec.exceptions.InvalidTokenException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import com.ftec.exceptions.InvalidTokenException;
-import com.ftec.services.TokenService;
-
+@SpringBootTest(classes = ApplicationConfig.class)
 @RunWith(SpringRunner.class)
 public class TokenServiceTest {
-    TokenService service = new TokenService(null);
+    @Autowired
+    public TokenService tokenService;
 
     @Test
     public void getValidIdFromTokenTest() {
