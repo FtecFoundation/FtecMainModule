1a5a33bbebea1d05cd5b07a25db684b72d0556b8
Roman <kovalovrmn@gmail.com>
2018-05-31 18:00:09 +0300

    get File method removed

--- src/main/java/com/ftec/controllers/AvatarController.java
+++ src/main/java/com/ftec/controllers/AvatarController.java
@@ -1,25 +1,9 @@
 package com.ftec.controllers;
 
-import com.ftec.entities.MvcResponse;
-import com.google.common.io.Files;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.File;
-import java.io.IOException;
 
 @RestController
 public class AvatarController {
 
-    @GetMapping(value = "/image")
-    public MvcResponse<byte[]> getFile() throws IOException {
-        File file = new File("C:/image.jpg");
-        byte[] byteObject = Files.toByteArray(file);
-        MvcResponse<byte[]> mvcResponse = new MvcResponse<>();
-        mvcResponse.setStatus(HttpStatus.OK);
-        mvcResponse.getResponse().put("image", byteObject);
 
-        return mvcResponse;
-    }
 }
