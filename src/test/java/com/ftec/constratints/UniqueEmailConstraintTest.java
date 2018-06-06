c2671c5a487421c66206508b8372e8850ff7c027
Ftec <ftecfound@gmail.com>
2018-06-06 21:49:37 +0300

    Try to make tests genericV2

--- src/test/java/com/ftec/constratints/UniqueEmailConstraintTest.java
+++ src/test/java/com/ftec/constratints/UniqueEmailConstraintTest.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@ActiveProfiles(inheritProfiles = false)
+@ActiveProfiles("test")
 @SpringBootTest(classes = ApplicationConfig.class)
 @RunWith(SpringRunner.class)
 public class UniqueEmailConstraintTest {
