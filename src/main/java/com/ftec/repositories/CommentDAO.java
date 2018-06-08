6366f534ce91dd09c5760cba70bad4085566b300
Roman <kovalovrmn@gmail.com>
2018-06-08 16:29:55 +0300

    Comment DAO Test added

--- src/main/java/com/ftec/repositories/CommentDAO.java
+++ src/main/java/com/ftec/repositories/CommentDAO.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 
@@ -12,6 +13,10 @@ import java.util.Date;
 public interface CommentDAO extends CrudRepository<Comment, Long> {
 
     @Modifying
+    @Transactional
     @Query(value = "INSERT INTO comment (ticket_id, creation_date, message, user_id) VALUES (?1, ?2, ?3, ?4);", nativeQuery = true)
     void saveCommentToTicket(long ticketId, Date creationDate, String message, long userId);
+
+    @Query(value = "SELECT * FROM comment WHERE ticket_id = ?1", nativeQuery = true)
+    Comment findByTicketId(long id);
 }
