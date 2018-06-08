8eece2b65b9c6c313cdab44071137139f2b29d8b
Roman <kovalovrmn@gmail.com>
2018-06-08 12:07:10 +0300

    Controller added for Tickets

--- src/main/java/com/ftec/entities/Ticket.java
+++ src/main/java/com/ftec/entities/Ticket.java
@@ -3,6 +3,7 @@ package com.ftec.entities;
 import com.ftec.resources.enums.TicketStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -11,6 +12,7 @@ import javax.validation.constraints.Size;
 @Data
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 @Table
 public class Ticket {
 
