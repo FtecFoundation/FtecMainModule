19ad91e7282a14fa4628363db77db3277cae6374
Roman <kovalovrmn@gmail.com>
2018-06-12 17:41:23 +0300

    Moved all Tests from ControllerTest to RegistrationControllerTest, ControllerTest removed

--- src/test/java/com/ftec/controllers/RegistrationControllerTest.java
+++ src/test/java/com/ftec/controllers/RegistrationControllerTest.java
@@ -39,9 +39,44 @@ public class RegistrationControllerTest {
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
+    @Test
+    public void createValidUser() throws Exception {
+        RegistrationController.UserRegistration userRegistration = EntityGenerator.getNewRegisrtUser();
+
+        mvc.perform(MockMvcRequestBuilders.post("/registration").
+                content(objectMapper.writeValueAsString(userRegistration)).contentType(MediaType.APPLICATION_JSON).
+                accept(MediaType.APPLICATION_JSON))
+                .andDo(Resources.doPrintStatic ? print() : (ResultHandler) result -> {}).andExpect(status().is(200));
+
+        assertTrue(userDAO.findByUsername(userRegistration.getUsername()).isPresent());
+    }
 
     @Test
-    public void trySaveNullUsername() throws Exception {
+    public void registerTwoValidUsers() throws Exception {
+
+        RegistrationController.UserRegistration userRegistration1 = EntityGenerator.getNewRegisrtUser();
+        RegistrationController.UserRegistration userRegistration2 = EntityGenerator.getNewRegisrtUser();
+
+        assertFalse(userDAO.findByUsername(userRegistration1.getUsername()).isPresent());
+        assertFalse(userDAO.findByUsername(userRegistration2.getUsername()).isPresent());
+
+        mvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/registration").
+                content(objectMapper.writeValueAsString(userRegistration1)).contentType(MediaType.APPLICATION_JSON).
+                accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(200));
+
+        mvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/registration").
+                content(objectMapper.writeValueAsString(userRegistration2)).contentType(MediaType.APPLICATION_JSON).
+                accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(200));
+
+        assertTrue(userDAO.findByUsername(userRegistration1.getUsername()).isPresent());
+        assertTrue(userDAO.findByUsername(userRegistration2.getUsername()).isPresent());
+
+    }
+
+    @Test
+    public void trySaveWithNullUsername() throws Exception {
         RegistrationController.UserRegistration userRegistration = new RegistrationController.UserRegistration();
         userRegistration.setUsername(null);
         userRegistration.setPassword("NullUser_Pass_228");
@@ -58,7 +93,7 @@ public class RegistrationControllerTest {
     }
 
     @Test
-    public void trySaveShortUsername() throws Exception {
+    public void trySaveWithShortUsername() throws Exception {
         RegistrationController.UserRegistration userRegistration = new RegistrationController.UserRegistration();
         userRegistration.setUsername("Lol");
         userRegistration.setPassword("ShortUser_Pass_228");
@@ -82,15 +117,133 @@ public class RegistrationControllerTest {
         mvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/registration").
                 content(objectMapper.writeValueAsString(userRegistration)).contentType(MediaType.APPLICATION_JSON).
                 accept(MediaType.APPLICATION_JSON))
-                .andDo(Resources.doPrintStatic ? print() : (ResultHandler) result -> {}).andExpect(status().is(200));
+                .andDo(Resources.doPrintStatic ? print() : (ResultHandler) result -> {
+                }).andExpect(status().is(200));
 
-        //should be status BadRequest
+        //should be status 400
         mvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/registration").
                 content(objectMapper.writeValueAsString(userRegistration)).contentType(MediaType.APPLICATION_JSON).
                 accept(MediaType.APPLICATION_JSON))
-                .andDo(Resources.doPrintStatic ? print() : (ResultHandler) result -> {}).andExpect(status().isBadRequest());
+                .andDo(Resources.doPrintStatic ? print() : (ResultHandler) result -> {
+                }).andExpect(status().is(400));
 
         assertTrue(userService.isDuplicateUserName(userName));
+    }
+
+    @Test
+    public void trySaveWithNullPassword() throws Exception {
+        RegistrationController.UserRegistration userRegistration = new RegistrationController.UserRegistration();
+        userRegistration.setUsername("NullPasswordUser");
+        userRegistration.setPassword(null);
+        userRegistration.setEmail("NullPassword_@gmail.com");
+
+        //should be status 400
+        mvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/registration").
+                content(objectMapper.writeValueAsString(userRegistration)).contentType(MediaType.APPLICATION_JSON).
+                accept(MediaType.APPLICATION_JSON))
+                .andDo(Resources.doPrintStatic ? print() : (ResultHandler) result -> {
+                }).andExpect(status().is(400));
+
+        assertFalse(userDAO.findByUsername(userRegistration.getUsername()).isPresent());
+    }
+
+    @Test
+    public void trySaveWithShortPassword() throws Exception {
+        RegistrationController.UserRegistration userRegistration = new RegistrationController.UserRegistration();
+        userRegistration.setUsername("ShortPasswordUser");
+        userRegistration.setPassword("lol");
+        userRegistration.setEmail("ShortPassword_@gmail.com");
+
+        //should be status 400
+        mvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/registration").
+                content(objectMapper.writeValueAsString(userRegistration)).contentType(MediaType.APPLICATION_JSON).
+                accept(MediaType.APPLICATION_JSON))
+                .andDo(Resources.doPrintStatic ? print() : (ResultHandler) result -> {
+                }).andExpect(status().is(400));
+
+        assertFalse(userDAO.findByUsername(userRegistration.getUsername()).isPresent());
+    }
+
+    @Test
+    public void trySaveWithWrongPasswordPattern() throws Exception {
+        RegistrationController.UserRegistration userRegistration = new RegistrationController.UserRegistration();
+        userRegistration.setUsername("WrongPassPatternUser");
+        userRegistration.setPassword("password");
+        userRegistration.setEmail("WrongPatternPass_@gmail.com");
+
+        //should be status 400
+        mvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/registration").
+                content(objectMapper.writeValueAsString(userRegistration)).contentType(MediaType.APPLICATION_JSON).
+                accept(MediaType.APPLICATION_JSON))
+                .andDo(Resources.doPrintStatic ? print() : (ResultHandler) result -> {
+                }).andExpect(status().is(400));
+
+        assertFalse(userDAO.findByUsername(userRegistration.getUsername()).isPresent());
+    }
+
+    @Test
+    public void trySaveWithNullEmail() throws Exception {
+        RegistrationController.UserRegistration userRegistration = new RegistrationController.UserRegistration();
+        userRegistration.setUsername("NullEmailUser");
+        userRegistration.setPassword("Strongpass1");
+        userRegistration.setEmail(null);
+
+        //should be status 400
+        mvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/registration").
+                content(objectMapper.writeValueAsString(userRegistration)).contentType(MediaType.APPLICATION_JSON).
+                accept(MediaType.APPLICATION_JSON))
+                .andDo(Resources.doPrintStatic ? print() : (ResultHandler) result -> {
+                }).andExpect(status().is(400));
+
+        assertFalse(userDAO.findByUsername(userRegistration.getUsername()).isPresent());
+    }
+
+    @Test
+    public void trySaveWithLongEmail() throws Exception {
+        RegistrationController.UserRegistration userRegistration = new RegistrationController.UserRegistration();
+        userRegistration.setUsername("LongEmailUser");
+        userRegistration.setPassword("Strongpass1");
+        userRegistration.setEmail("VeryVeryVeryVeryVeryVeryVeryLong@gmail.com");
+
+        //should be status 400
+        mvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/registration").
+                content(objectMapper.writeValueAsString(userRegistration)).contentType(MediaType.APPLICATION_JSON).
+                accept(MediaType.APPLICATION_JSON))
+                .andDo(Resources.doPrintStatic ? print() : (ResultHandler) result -> {
+                }).andExpect(status().is(400));
+
+        assertFalse(userDAO.findByUsername(userRegistration.getUsername()).isPresent());
+    }
+
+    @Test
+    public void trySaveDuplicateEmail() throws Exception {
+        String email = "trySaveDuplicate@mail.com";
+
+        RegistrationController.UserRegistration userRegistration1 = new RegistrationController.UserRegistration();
+        userRegistration1.setUsername("OriginalEmailUser");
+        userRegistration1.setPassword("Strongpass1");
+        userRegistration1.setEmail(email);
+
+        RegistrationController.UserRegistration userRegistration2 = new RegistrationController.UserRegistration();
+        userRegistration1.setUsername("DuplicateEmailUser");
+        userRegistration1.setPassword("Strongpass1");
+        userRegistration1.setEmail(email);
+
+        mvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/registration").
+                content(objectMapper.writeValueAsString(userRegistration1)).contentType(MediaType.APPLICATION_JSON).
+                accept(MediaType.APPLICATION_JSON))
+                .andDo(Resources.doPrintStatic ? print() : (ResultHandler) result -> {
+                }).andExpect(status().is(200));
+
+        //should be status 400
+        mvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/registration").
+                content(objectMapper.writeValueAsString(userRegistration2)).contentType(MediaType.APPLICATION_JSON).
+                accept(MediaType.APPLICATION_JSON))
+                .andDo(Resources.doPrintStatic ? print() : (ResultHandler) result -> {
+                }).andExpect(status().is(400));
+
+        assertTrue(userDAO.findByUsername(userRegistration1.getUsername()).isPresent());
+        assertFalse(userDAO.findByUsername(userRegistration2.getUsername()).isPresent());
     }
 
 }
